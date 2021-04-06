7a8925fca5a7fe79133ce682bbcaf0a63eae0c10
sekyungk <sekyungk@outlook.com>
2021-04-06 14:47:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reat
+          {process.env.RMS_EXT_API}
         </a>
       </header>
     </div>

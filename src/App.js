4c07e662529b2f49bbb901d077a54702c9e97987
sekyungk <sekyungk@outlook.com>
2021-04-13 19:55:17 +0900

    adf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {process.env.REACT_APP_TEST}
+          test
         </a>
       </header>
     </div>
